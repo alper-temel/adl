dca73716138668ef77eacd81d66299f2adbc868a
alper-temel <65859199+alper-temel@users.noreply.github.com>
2023-03-23 17:33:32 +0300

    Update master.py

--- master.py
+++ master.py
@@ -29,7 +29,7 @@ if uploaded_excel is not None:
 else:
     st.caption("Lütfen Excel Dosyasını Kontrol Edin")
     
-@st.cache_data
+
 def ADL():
     if uploaded_excel is not None:
          uploaded_excel.seek(0)
