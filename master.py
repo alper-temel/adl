df6e7a6f55d2c357b5145466e02b5941dbf282d5
alper-temel <65859199+alper-temel@users.noreply.github.com>
2023-03-23 17:17:48 +0300

    Update master.py

--- master.py
+++ master.py
@@ -320,4 +320,4 @@ def ADL():
     return adl_dilim
 
 
-st.download_button(label = "ADL İndir :rocket:", on_click = ADL, file_name = "Dilim.xlsx", data = ADL())
+st.download_button(label = "ADL İndir :rocket:", file_name = "Dilim.xlsx", data = ADL())
